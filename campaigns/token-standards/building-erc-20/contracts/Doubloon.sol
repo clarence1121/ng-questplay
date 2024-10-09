adecc4ebeb0b60d2c000f7a1fce160cdd1e923e2
clarence <kevin25731568@gmail.com>
2024-10-09 17:13:53 +0800

    1

--- campaigns/token-standards/building-erc-20/contracts/Doubloon.sol
+++ campaigns/token-standards/building-erc-20/contracts/Doubloon.sol
@@ -7,7 +7,78 @@ import "./interfaces/IERC20.sol";
  * @dev ERC-20 token contract.
  */
 contract Doubloon /* is IERC20 */ {
+    uint256 internal _totalSupply;
+    string private _symbol;
+    string private _name;
+    address owner;
+    // 儲存每個地址的餘額
+    mapping(address => uint256) internal _balances;
+    // 儲存授權的金額：擁有者 => (被授權人 => 金額)
+    mapping(address => mapping(address => uint256)) internal _allowances;
 
-    constructor(uint256 _supply) { }
+    // 事件定義：Transfer 和 Approval
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
 
+    constructor(uint256 initialSupply) {
+        _symbol = "DBL";
+        _name = "Doubloon";
+        _totalSupply = initialSupply;
+        _balances[msg.sender] = initialSupply; // 分配全部供應量給合約創建者
+        owner = msg.sender;
+        emit Transfer(address(0), msg.sender, initialSupply);
+    }
+
+    function name() public view returns (string memory) {
+        return _name;
+    }
+
+    function symbol() public view returns (string memory) {
+        return _symbol;
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) external view returns (uint256) {
+        return _balances[account];
+    }
+
+    function transfer(address to, uint256 amount) external returns (bool) {
+        require(to != address(0), "Transfer to the zero address");
+        require(_balances[msg.sender] >= amount, "Insufficient balance");
+
+        _balances[msg.sender] -= amount;
+        _balances[to] += amount;
+
+        emit Transfer(msg.sender, to, amount);
+        return true;
+    }
+
+    function approve(address spender, uint256 amount) external returns (bool) {
+        require(spender != address(0), "Approve to the zero address");
+
+        _allowances[msg.sender][spender] = amount;
+
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 amount) external returns (bool) {
+        require(to != address(0), "Transfer to the zero address");
+        require(_balances[from] >= amount, "Insufficient balance");
+        require(_allowances[from][msg.sender] >= amount, "Transfer amount exceeds allowance");
+
+        _balances[from] -= amount;
+        _balances[to] += amount;
+        _allowances[from][msg.sender] -= amount;
+
+        emit Transfer(from, to, amount);
+        return true;
+    }
+
+    function allowance(address owner, address spender) external view returns (uint256) {
+        return _allowances[owner][spender];
+    }
 }
--- campaigns/token-standards/building-erc-20/contracts/MintableDoubloon.sol
+++ campaigns/token-standards/building-erc-20/contracts/MintableDoubloon.sol
@@ -6,5 +6,11 @@ import "./Doubloon.sol";
 contract MintableDoubloon is Doubloon {
 
     constructor(uint256 _supply) Doubloon(_supply) {}
-
+    function mint(address _to, uint256 _amount) external {
+        require(msg.sender==owner,"not owner");
+         _totalSupply += _amount;
+        _balances[_to]+=_amount;
+        emit Transfer(address(0), _to, _amount); 
+    }
 }
+
