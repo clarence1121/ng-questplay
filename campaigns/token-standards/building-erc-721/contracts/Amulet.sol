559e3319f488c79c4dac266289b5dd68bcb4f194
clarence <kevin25731568@gmail.com>
2024-10-09 18:28:47 +0800

    1

--- campaigns/token-standards/building-erc-721/contracts/Amulet.sol
+++ campaigns/token-standards/building-erc-721/contracts/Amulet.sol
@@ -9,6 +9,266 @@ import "./interfaces/IERC721TokenReceiver.sol";
 /**
  * @dev ERC-721 token contract.
  */
-contract Amulet /* is IERC721, IERC721Metadata */ {
+contract Amulet {
+    // 合約名稱與符號
+    string private _name;
+    string private _symbol;
 
+    // Token ID 計數器
+    uint256 private _currentTokenId;
+
+    // Token ID 到擁有者地址的映射
+    mapping(uint256 => address) private _owners;
+
+    // 擁有者地址到其擁有的 Token 數量的映射
+    mapping(address => uint256) private _balances;
+
+    // Token ID 到授權地址的映射
+    mapping(uint256 => address) private _tokenApprovals;
+
+    // 擁有者地址到授權地址到授權金額的映射
+    mapping(address => mapping(address => bool)) private _operatorApprovals;
+
+    // Token ID 到其 URI 的映射
+    mapping(uint256 => string) private _tokenURIs;
+
+    // 擁有者地址
+    address private _owner;
+
+    // ERC-165 支持的接口 ID
+    bytes4 private constant _INTERFACE_ID_ERC165 = 0x01ffc9a7;
+    bytes4 private constant _INTERFACE_ID_ERC721 = 0x80ac58cd;
+    bytes4 private constant _INTERFACE_ID_ERC721_METADATA = 0x5b5e139f;
+
+    /**
+     * @dev 事件定義
+     */
+    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
+    event Approval(address indexed owner, address indexed approved, uint256 indexed tokenId);
+    event ApprovalForAll(address indexed owner, address indexed operator, bool approved);
+
+    /**
+     * @dev 構造函數，設置名稱和符號，並設置合約創建者為擁有者
+     */
+    constructor() {
+        _name = "Amulet";
+        _symbol = "AMULET";
+        _owner = msg.sender;
+        _currentTokenId = 0;
+    }
+
+    /**
+     * @dev ERC-165 標準支持的實現
+     */
+    function supportsInterface(bytes4 interfaceId) external view returns (bool) {
+        return
+            interfaceId == _INTERFACE_ID_ERC165 ||
+            interfaceId == _INTERFACE_ID_ERC721 ||
+            interfaceId == _INTERFACE_ID_ERC721_METADATA;
+    }
+
+    /**
+     * @dev 返回合約名稱
+     */
+    function name() external view returns (string memory) {
+        return _name;
+    }
+
+    /**
+     * @dev 返回合約符號
+     */
+    function symbol() external view returns (string memory) {
+        return _symbol;
+    }
+
+    /**
+     * @dev 返回指定 Token ID 的 URI
+     * Reverts if Token ID 無效
+     */
+    function tokenURI(uint256 tokenId) external view returns (string memory) {
+        require(_owners[tokenId]!=address(0), "Amulet: URI query for nonexistent token");
+        return _tokenURIs[tokenId];
+    }
+
+    /**
+     * @dev 返回指定地址擁有的 Token 數量
+     */
+    function balanceOf(address owner) external view returns (uint256) {
+        require(owner != address(0), "Amulet: balance query for the zero address");
+        return _balances[owner];
+    }
+
+    /**
+     * @dev 返回指定 Token ID 的擁有者
+     * Reverts if Token ID 無效
+     */
+    function ownerOf(uint256 tokenId) external view  returns (address) {
+        address owner = _owners[tokenId];
+        require(owner != address(0), "Amulet: owner query for nonexistent token");
+        return owner;
+    }
+
+    /**
+     * @dev 授權指定地址管理指定 Token ID
+     */
+    function approve(address to, uint256 tokenId) external {
+        //先確定你是token id 的owner 或者是owner有授權跟給妳
+        require(msg.sender==_owners[tokenId] || _operatorApprovals[msg.sender][to] , "not owner of no approve");
+        _approve(to, tokenId);
+    }
+
+    /**
+     * @dev 返回指定 Token ID 的授權地址
+     */
+    function getApproved(uint256 tokenId) internal view  returns (address) {
+        require(_exists(tokenId), "Amulet: approved query for nonexistent token");
+        return _tokenApprovals[tokenId];
+    }
+
+    /**
+     * @dev 設置或取消授權操作員管理所有者的所有 Token
+     */
+    function setApprovalForAll(address operator, bool approved) external  {
+        require(operator != msg.sender, "Amulet: approve to caller");
+
+        _operatorApprovals[msg.sender][operator] = approved;
+        emit ApprovalForAll(msg.sender, operator, approved);
+    }
+
+    /**
+     * @dev 返回操作員是否被授權管理所有者的所有 Token
+     */
+    function isApprovedForAll(address owner, address operator) public view  returns (bool) {
+        return _operatorApprovals[owner][operator];
+    }
+
+    /**
+     * @dev 轉移指定 Token ID 從一個地址到另一個地址
+     */
+    function transferFrom(address from, address to, uint256 tokenId) external  {
+        require(_isApprovedOrOwner(msg.sender, tokenId), "Amulet: transfer caller is not owner nor approved");
+        _transfer(from, to, tokenId);
+    }
+
+    /**
+     * @dev 安全的轉移指定 Token ID，檢查接收者是否支持 ERC721 接口
+     */
+    function safeTransferFrom(address from, address to, uint256 tokenId) external {
+        safeTransferFrom(from, to, tokenId, "");
+    }
+
+    /**
+     * @dev 安全的轉移指定 Token ID，並附加數據
+     */
+    function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory _data) public  {
+        require(_isApprovedOrOwner(msg.sender, tokenId), "Amulet: transfer caller is not owner nor approved");
+        _safeTransfer(from, to, tokenId, _data);
+    }
+
+    /**
+     * @dev 鑄造一個新的 Amulet，並分配給指定地址
+     * 只有合約創建者可以調用
+     */
+    function mint(address to, string memory uri) external returns (uint256) {
+        require(msg.sender == _owner, "Amulet: only owner can mint");
+        require(to != address(0), "Amulet: mint to the zero address");
+
+        uint256 tokenId = _currentTokenId;
+        _currentTokenId += 1;
+
+        _balances[to] += 1;
+        _owners[tokenId] = to;
+        _tokenURIs[tokenId] = uri;
+
+        emit Transfer(address(0), to, tokenId);
+
+        return tokenId;
+    }
+
+    /**
+     * @dev 內部函數，安全地轉移 Token，並檢查接收者是否支持 ERC721 接口
+     */
+    function _safeTransfer(address from, address to, uint256 tokenId, bytes memory _data) internal {
+        _transfer(from, to, tokenId);
+        require(_checkOnERC721Received(from, to, tokenId, _data), "Amulet: transfer to non ERC721Receiver implementer");
+    }
+
+    /**
+     * @dev 內部函數，轉移 Token
+     */
+    function _transfer(address from, address to, uint256 tokenId) internal {
+        require(_owners[tokenId] == from, "Amulet: transfer of token that is not own");
+        require(to != address(0), "Amulet: transfer to the zero address");
+
+        // 清除之前的授權
+        _approve(address(0), tokenId);
+
+        // 更新餘額和擁有者
+        _balances[from] -= 1;
+        _balances[to] += 1;
+        _owners[tokenId] = to;
+
+        emit Transfer(from, to, tokenId);
+    }
+
+    /**
+     * @dev 內部函數，授權指定地址管理 Token
+     */
+    function _approve(address to, uint256 tokenId) internal {
+        _tokenApprovals[tokenId] = to;
+        emit Approval(_owners[tokenId], to, tokenId);
+    }
+
+    /**
+     * @dev 內部函數，檢查是否授權或擁有者
+     */
+    function _isApprovedOrOwner(address spender, uint256 tokenId) internal view returns (bool) {
+        require(_exists(tokenId), "Amulet: operator query for nonexistent token");
+        address owner = _owners[tokenId];
+        return (spender == owner || getApproved(tokenId) == spender || isApprovedForAll(owner, spender));
+    }
+
+    /**
+     * @dev 內部函數，檢查 Token 是否存在
+     */
+    function _exists(uint256 tokenId) internal view returns (bool) {
+        return _owners[tokenId] != address(0);
+    }
+
+    /**
+     * @dev 內部函數，檢查接收者是否支持 ERC721 接口
+     */
+    function _checkOnERC721Received(address from, address to, uint256 tokenId, bytes memory _data)
+        internal
+        returns (bool)
+    {
+        if (isContract(to)) {
+            try IERC721TokenReceiver(to).onERC721Received(msg.sender, from, tokenId, _data) returns (bytes4 retval) {
+                return retval == IERC721TokenReceiver.onERC721Received.selector;
+            } catch (bytes memory reason) {
+                if (reason.length == 0) {
+                    revert("Amulet: transfer to non ERC721Receiver implementer");
+                } else {
+                    // 向上拋出錯誤訊息
+                    assembly {
+                        revert(add(32, reason), mload(reason))
+                    }
+                }
+            }
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * @dev 內部函數，檢查地址是否為合約
+     */
+    function isContract(address account) internal view returns (bool) {
+        uint256 size;
+        assembly {
+            size := extcodesize(account)
+        }
+        return size > 0;
+    }
 }
+
--- campaigns/token-standards/building-erc-721/contracts/AmuletPouch.sol
+++ campaigns/token-standards/building-erc-721/contracts/AmuletPouch.sol
@@ -5,10 +5,176 @@ import "./interfaces/IERC721.sol";
 import "./interfaces/IAmuletPouch.sol";
 
 /**
- * @dev ERC-721 Token Receiver token contract.
+ * @dev ERC-721 Token Receiver contract for AmuletPouch.
  */
-contract AmuletPouch /* is IAmuletPouch */ {
+contract AmuletPouch {
+    // Amulet 合約地址
+    IERC721 public immutable amulet;
 
-    constructor(address _amulet) { }
+    // 成員列表
+    mapping(address => bool) private _members;
+    address[] private _memberList;
 
+    // 提現請求結構
+    struct WithdrawRequest {
+        address requester;
+        uint256 tokenId;
+        uint256 votes;
+        bool exists;
+        mapping(address => bool) hasVoted;
+    }
+
+    // 所有提現請求，使用 requestId 作為鍵
+    mapping(uint256 => WithdrawRequest) private _withdrawRequests;
+    uint256 private _currentRequestId;
+
+    // 事件定義
+    event WithdrawRequested(address indexed requester, uint256 indexed tokenId, uint256 indexed requestId);
+
+    /**
+     * @dev 構造函數，設置 Amulet 合約地址。
+     * @param _amuletAddress Amulet 合約地址。
+     */
+    constructor(address _amuletAddress) {
+        require(_amuletAddress != address(0), "AmuletPouch: invalid Amulet address");
+        amulet = IERC721(_amuletAddress);
+        _currentRequestId = 0;
+    }
+
+    /**
+     * @dev 返回是否為成員。
+     * @param _user 要檢查的地址。
+     */
+    function isMember(address _user) external view returns (bool) {
+        return _members[_user];
+    }
+
+    /**
+     * @dev 返回總成員數量。
+     */
+    function totalMembers() external view returns (uint256) {
+        return _memberList.length;
+    }
+
+    /**
+     * @dev 返回指定 requestId 的請求信息。
+     * @param _requestId 提現請求的 ID。
+     */
+    function withdrawRequest(uint256 _requestId) external view returns (address, uint256) {
+        require(_withdrawRequests[_requestId].exists, "AmuletPouch: request does not exist");
+       WithdrawRequest storage temp =  _withdrawRequests[_requestId];
+        return (temp.requester, temp.tokenId);
+    }
+
+    /**
+     * @dev 返回指定 requestId 的投票數量。
+     * @param _requestId 提現請求的 ID。
+     */
+    function numVotes(uint256 _requestId) external view  returns (uint256) {
+        require(_withdrawRequests[_requestId].exists, "AmuletPouch: request does not exist");
+        return _withdrawRequests[_requestId].votes;
+    }
+
+    /**
+     * @dev 成員對指定的提現請求進行投票。
+     * @param _requestId 提現請求的 ID。
+     */
+    function voteFor(uint256 _requestId) external  {
+        require(_members[msg.sender], "AmuletPouch: caller is not a member");
+        WithdrawRequest storage request = _withdrawRequests[_requestId];
+        require(request.exists, "AmuletPouch: request does not exist");
+        require(!request.hasVoted[msg.sender], "AmuletPouch: already voted for this request");
+
+        // 註記已投票
+        request.hasVoted[msg.sender] = true;
+        request.votes += 1;
+
+        // 如果是請求者，默認投票
+        // 根據描述，提請者已經隱含投票，所以在創建請求時應該已經投票
+        // 此處不需要額外處理
+
+        // 不需要觸發事件
+    }
+
+    /**
+     * @dev 處理指定的提現請求，將 Amulet 轉移給請求者。
+     * @param _requestId 提現請求的 ID。
+     */
+    function withdraw(uint256 _requestId) external  {
+        WithdrawRequest storage request = _withdrawRequests[_requestId];
+        require(request.exists, "AmuletPouch: request does not exist");
+        require(msg.sender == request.requester, "AmuletPouch: caller is not the requester");
+
+        uint256 majority = (_memberList.length / 2) + 1;
+        require(request.votes >= majority, "AmuletPouch: not enough votes");
+
+        // 清除請求
+        delete _withdrawRequests[_requestId];
+
+        // 轉移 Amulet 給請求者
+        amulet.safeTransferFrom(address(this), msg.sender, request.tokenId);
+    }
+
+    /**
+     * @dev ERC-721 接受者回調函數，處理接收到的 Amulet。
+     * @param _operator 操作員地址。
+     * @param _from 發送者地址。
+     * @param _tokenId 接收到的 Amulet 的 ID。
+     * @param _data 附加的數據，用於提現請求。
+     * @return bytes4 返回接受者標識符。
+     */
+    function onERC721Received(
+        address _operator,
+        address _from,
+        uint256 _tokenId,
+        bytes calldata _data
+    ) external  returns (bytes4) {
+        require(msg.sender == address(amulet), "AmuletPouch: only Amulet tokens are accepted");
+
+        if (!_members[_from]) {
+            // 新成員，添加到成員列表
+            _members[_from] = true;
+            _memberList.push(_from);
+        } else {
+            if (_data.length > 0) {
+                // 提現請求
+                require(_data.length == 32, "AmuletPouch: invalid data length");
+
+                uint256 requestedTokenId;
+                // 從 _data 中提取 tokenId
+                assembly {
+                    requestedTokenId := calldataload(132) // _data starts at offset 132
+                }
+
+                // 創建新的提現請求
+                uint256 requestId = _currentRequestId;
+                WithdrawRequest storage newRequest = _withdrawRequests[requestId];
+                newRequest.requester = _from;
+                newRequest.tokenId = requestedTokenId;
+                newRequest.votes = 1; // 提請者隱含投票
+                newRequest.exists = true;
+                newRequest.hasVoted[_from] = true;
+
+                emit WithdrawRequested(_from, requestedTokenId, requestId);
+
+                _currentRequestId += 1;
+            }
+        }
+
+        return this.onERC721Received.selector;
+    }
+
+    /**
+     * @dev 返回當前提現請求的數量。
+     */
+    function getCurrentRequestId() external view returns (uint256) {
+        return _currentRequestId;
+    }
+
+    /**
+     * @dev 返回所有成員地址（僅用於測試或前端顯示）。
+     */
+    function getAllMembers() external view returns (address[] memory) {
+        return _memberList;
+    }
 }
