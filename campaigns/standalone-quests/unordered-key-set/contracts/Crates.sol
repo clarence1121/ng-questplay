24ca4d1dc3678c301e5f35f8c602c4e9b5f7769d
clarence <kevin25731568@gmail.com>
2024-10-11 14:39:08 +0800

    a

--- campaigns/standalone-quests/unordered-key-set/contracts/Crates.sol
+++ campaigns/standalone-quests/unordered-key-set/contracts/Crates.sol
@@ -4,59 +4,66 @@ pragma solidity ^0.8.19;
 import './interfaces/ICrates.sol';
 
 contract Crates is ICrates {
-    struct crates{
+    struct Crate {
         uint size;
         uint strength;
+        uint index; // 存储在crateIds数组中的位置
     }
-    mapping(uint=>crates) public crates_mapping;
-    uint[] internal is_init;//用來記錄id存不存在
 
-    //建立一個mapping把id對到他對應的index
-    mapping(uint=>uint) internal id_index;
+    mapping(uint => Crate) private crates;
+    uint[] private crateIds;
 
-        // CODE HERE
-    /// @notice Inserts a crate into the contract. Fails if id belongs to an existing crate.
+    /// @notice 插入一个crate。如果ID已存在，则操作失败。
     function insertCrate(
-        uint id, 
-        uint size, 
+        uint id,
+        uint size,
         uint strength
-    ) external{
-        //mapping(uint=>crates) storage temp = crates_mapping;
-        
-        require(size>=0 && strength>=0 && id>=0,"not valid input");
-        require(crates_mapping[id].size==0&&crates_mapping[id].strength==0,"already existing");
-        crates_mapping[id] = crates(size , strength);
-        is_init.push(id);
-        id_index[id] = is_init.length;
+    ) external {
+        require(!_isCrate(id), "Already existing");
+
+        crates[id] = Crate(size, strength, crateIds.length);
+        crateIds.push(id);
     }
 
-   /// @notice Retrieves a crate based on id. Fails if id does not belong to an existing crate.
+    /// @notice 根据ID检索crate。如果ID不存在，则操作失败。
     function getCrate(uint id) external view returns (uint size, uint strength) {
-        crates memory crate = crates_mapping[id];
-        require(crate.size != 0 || crate.strength != 0, "Crate does not exist");
-        
+        require(_isCrate(id), "Crate does not exist");
+        Crate storage crate = crates[id];
         return (crate.size, crate.strength);
     }
 
-    /// @notice Retrieve the IDs of all existing crates.
+    /// @notice 检索所有现有crate的ID。
     function getCrateIds() external view returns (uint[] memory) {
-        return is_init;
+        return crateIds;
     }
-    /// @notice Delete a crate by id. Fails if id doesn't belong to an existing crate.
-    function deleteCrate(uint id) external {
-        require(crates_mapping[id].size != 0 || crates_mapping[id].strength != 0, "Crate does not exist");
 
-        delete crates_mapping[id];
-        is_init[id_index[id]] = is_init[is_init.length-1];
-        is_init.pop;
-        delete id_index[id];
-        // // Remove id from crateIds array (not optimal, but simple)
-        // for (uint i = 0; i < is_init.length; i++) {
-        //     if (is_init[i] == id) {
-        //         is_init[i] = is_init[is_init.length - 1];
-        //         is_init.pop();
-        //         break;
-        //     }
-        // }
+    /// @notice 根据ID删除crate。如果ID不存在，则操作失败。
+    function deleteCrate(uint id) external {
+        require(_isCrate(id), "Crate does not exist");
+
+        uint index = crates[id].index;
+        uint lastIndex = crateIds.length - 1;
+        uint lastId = crateIds[lastIndex];
+
+        // 将最后一个元素移动到要删除的位置
+        crateIds[index] = lastId;
+        crates[lastId].index = index;
+
+        // 移除最后一个元素
+        crateIds.pop();
+
+        // 删除crate映射中的数据
+        //delete crates[id];
+    }
+
+    /// @notice 私有函数，检查crate是否存在
+    function _isCrate(uint id) private view returns (bool) {
+        uint index = crates[id].index;
+
+        if (index >= crateIds.length) {
+            return false;
+        }
+
+        return crateIds[index] == id;
     }
 }
