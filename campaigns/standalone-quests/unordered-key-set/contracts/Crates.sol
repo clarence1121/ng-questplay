aa56404419877060325c1105cb139b91433a054c
clarence <kevin25731568@gmail.com>
2024-10-11 13:53:35 +0800

    1

--- campaigns/standalone-quests/unordered-key-set/contracts/Crates.sol
+++ campaigns/standalone-quests/unordered-key-set/contracts/Crates.sol
@@ -4,5 +4,51 @@ pragma solidity ^0.8.19;
 import './interfaces/ICrates.sol';
 
 contract Crates is ICrates {
-    // CODE HERE
+    struct crates{
+        uint size;
+        uint strength;
+    }
+    mapping(uint=>crates) public crates_mapping;
+    uint[] internal is_init;//用來記錄id存不存在
+        // CODE HERE
+    /// @notice Inserts a crate into the contract. Fails if id belongs to an existing crate.
+    function insertCrate(
+        uint id, 
+        uint size, 
+        uint strength
+    ) external{
+        //mapping(uint=>crates) storage temp = crates_mapping;
+        
+        require(size>=0 && strength>=0 && id>=0,"not valid input");
+        require(crates_mapping[id].size==0&&crates_mapping[id].strength==0,"already existing");
+        crates_mapping[id] = crates(size , strength);
+        is_init.push(id);
+    }
+
+   /// @notice Retrieves a crate based on id. Fails if id does not belong to an existing crate.
+    function getCrate(uint id) external view returns (uint size, uint strength) {
+        crates memory crate = crates_mapping[id];
+        require(crate.size != 0 || crate.strength != 0, "Crate does not exist");
+        
+        return (crate.size, crate.strength);
+    }
+
+    /// @notice Retrieve the IDs of all existing crates.
+    function getCrateIds() external view returns (uint[] memory) {
+        return is_init;
+    }
+    /// @notice Delete a crate by id. Fails if id doesn't belong to an existing crate.
+    function deleteCrate(uint id) external {
+        require(crates_mapping[id].size != 0 || crates_mapping[id].strength != 0, "Crate does not exist");
+
+        delete crates_mapping[id];
+        // Remove id from crateIds array (not optimal, but simple)
+        for (uint i = 0; i < is_init.length; i++) {
+            if (is_init[i] == id) {
+                is_init[i] = is_init[is_init.length - 1];
+                is_init.pop();
+                break;
+            }
+        }
+    }
 }
